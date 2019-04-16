375b15bc887965a4d70cd7b5350581c2d596cdba
Dan Federman <dan.federman@airbnb.com>
2019-04-16 16:04:37 -0700

    Stop using sudo

--- gem/install_gems.sh
+++ gem/install_gems.sh
@@ -2,20 +2,14 @@
 
 set -e
 
-# Make sure we're the owner of /Library/Ruby/Gems/
-if [ $(stat -f '%Su' /Library/Ruby/Gems/) != "$USER" ]; then
-    echo "Setting ownership of /Library/Ruby/Gems/"
-    sudo chown -R $USER /Library/Ruby/Gems/
-fi
-
 # Install cocoapods if it doesn't exist
 if [ -z "$(gem list --no-versions | grep cocoapods)" ]; then
     echo "Installing cocoapods"
-    gem install cocoapods
+    gem install --user-install cocoapods
 fi
 
 # Install cocoapods if it doesn't exist
 if [ -z "$(gem list --no-versions | grep bundler)" ]; then
     echo "Installing bundler"
-    gem install bundler
+    gem install --user-install bundler
 fi
