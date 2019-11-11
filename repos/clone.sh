9aa05568c308735df8c6a9edf97d008d16f467ca
Dan Federman <dfed@me.com>
2019-11-10 18:33:30 -0800

    Add CacheAdvance, and sort

--- repos/clone.sh
+++ repos/clone.sh
@@ -12,10 +12,11 @@ function clone_repo_into_destination {
     fi
 }
 
+clone_repo_into_destination git@github.com:airbnb/BuckSample.git $HOME/Documents/source/BuckSample
+clone_repo_into_destination git@github.com:dfed/CacheAdvance.git $HOME/Documents/source/CacheAdvance
 clone_repo_into_destination git@github.com:dfed/FancyPantsPS1.git $HOME/Documents/source/FancyPantsPS1
 clone_repo_into_destination git@github.com:dfed/grb.git $HOME/Documents/source/grb
 clone_repo_into_destination git@github.com:dfed/Relativity.git $HOME/Documents/source/Relativity
 clone_repo_into_destination git@github.com:dfed/XCTest-watchOS.git $HOME/Documents/source/XCTest-watchOS
-clone_repo_into_destination git@github.com:Square/Aardvark.git $HOME/Documents/source/Aardvark
-clone_repo_into_destination git@github.com:Square/Valet.git $HOME/Documents/source/Valet
-clone_repo_into_destination git@github.com:Airbnb/BuckSample.git $HOME/Documents/source/BuckSample
+clone_repo_into_destination git@github.com:square/Aardvark.git $HOME/Documents/source/Aardvark
+clone_repo_into_destination git@github.com:square/Valet.git $HOME/Documents/source/Valet
