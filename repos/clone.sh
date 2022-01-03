2a76bf6bda818221f977e1e0b4b5cf959f041729
Dan Federman <dfed@me.com>
2022-01-03 10:06:03 -0800

    We don't need to clone grb

--- repos/clone.sh
+++ repos/clone.sh
@@ -15,7 +15,6 @@ function clone_repo_into_destination {
 clone_repo_into_destination git@github.com:airbnb/BuckSample.git $HOME/source/BuckSample
 clone_repo_into_destination git@github.com:dfed/CacheAdvance.git $HOME/source/CacheAdvance
 clone_repo_into_destination git@github.com:dfed/FancyPantsPS1.git $HOME/source/FancyPantsPS1
-clone_repo_into_destination git@github.com:dfed/grb.git $HOME/source/grb
 clone_repo_into_destination git@github.com:dfed/Relativity.git $HOME/source/Relativity
 clone_repo_into_destination git@github.com:dfed/XCTest-watchOS.git $HOME/source/XCTest-watchOS
 clone_repo_into_destination git@github.com:square/Aardvark.git $HOME/source/Aardvark
