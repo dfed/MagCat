3e7978440e5d1e964079a98969b9d87a3d73f42f
Dan Federman <dfed@me.com>
2022-10-29 00:51:56 -0700

    Add SwiftInspector to repos + simplify clone.sh

--- repos/clone.sh
+++ repos/clone.sh
@@ -6,7 +6,7 @@ echo "Cloning repos…"
 
 function clone_repo_into_destination {
     REPO=$1
-    DESTINATION=$2
+    DESTINATION=$HOME/source/$(basename $REPO)
 
     if [ ! -d "$DESTINATION" ]; then
         echo "Cloning $REPO"
@@ -19,10 +19,11 @@ if ! gh auth status -h github.com >/dev/null 2>&1; then
     gh auth login
 fi
 
-clone_repo_into_destination dfed/MagCat $HOME/source/MagCat
-clone_repo_into_destination dfed/CacheAdvance $HOME/source/CacheAdvance
-clone_repo_into_destination dfed/FancyPantsPS1 $HOME/source/FancyPantsPS1
-clone_repo_into_destination dfed/Relativity $HOME/source/Relativity
-clone_repo_into_destination dfed/XCTest-watchOS $HOME/source/XCTest-watchOS
-clone_repo_into_destination square/Aardvark $HOME/source/Aardvark
-clone_repo_into_destination square/Valet $HOME/source/Valet
+clone_repo_into_destination dfed/MagCat
+clone_repo_into_destination dfed/CacheAdvance
+clone_repo_into_destination dfed/FancyPantsPS1
+clone_repo_into_destination dfed/Relativity
+clone_repo_into_destination dfed/XCTest-watchOS $HOM
+clone_repo_into_destination fdiaz/SwiftInspector
+clone_repo_into_destination square/Aardvark
+clone_repo_into_destination square/Valet
