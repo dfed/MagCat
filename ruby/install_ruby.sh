48954bb19dd2d509ab3f10735b04876a2e30ce50
Dan Federman <dan@portola.ai>
2024-02-05 16:35:29 -0800

    Use ruby 3.1.4

--- ruby/install_ruby.sh
+++ ruby/install_ruby.sh
@@ -4,14 +4,12 @@ set -e
 
 echo "Inspecting Ruby…"
 
-rbenv install --list-all
-
-# Install Ruby 3.2.3 if it isn't already installed.
+# Install Ruby 3.1.4 if it isn't already installed.
 # This version of ruby works well with ARM-based Mac machines.
-# 3.2.3 was the latest stable at the time of writing.
+# 3.1.4 was the latest stable at the time of writing.
 # We need Ruby before we install our Gems.
-if [[ ! $(rbenv versions --bare) =~ 3.2.3 ]]; then
-    echo "Installing ruby 3.2.3"
-    rbenv install 3.2.3
-    rbenv global 3.2.3
+if [[ ! $(rbenv versions --bare) =~ 3.1.4 ]]; then
+    echo "Installing ruby 3.1.4"
+    rbenv install 3.1.4
+    rbenv global 3.1.4
 fi
