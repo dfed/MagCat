a1b55e2a44f0463e1438cddb3aee93c5fc79adea
Dan Federman <dfed@me.com>
2025-05-22 22:26:45 -0700

    simplify comment

--- ruby/install_ruby.sh
+++ ruby/install_ruby.sh
@@ -4,9 +4,7 @@ set -e
 
 echo "Inspecting Ruby…"
 
-# Install Ruby 3.4.4 if it isn't already installed.
-# This version of ruby works well with ARM-based Mac machines.
-# 3.4.4 was the latest stable at the time of writing.
+# Install Ruby if it isn't already installed.
 # We need Ruby before we install our Gems.
 if [[ ! $(rbenv versions --bare) =~ $1 ]]; then
 	echo "Installing ruby $1"
