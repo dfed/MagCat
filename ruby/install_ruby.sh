4bce5dc7e6c66c456b6eb52b8d746e84bdada467
Dan Federman <dfed@me.com>
2025-05-22 22:33:32 -0700

    better check

--- ruby/install_ruby.sh
+++ ruby/install_ruby.sh
@@ -6,7 +6,7 @@ echo "Inspecting Ruby…"
 
 # Install Ruby if it isn't already installed.
 # We need Ruby before we install our Gems.
-if [[ ! $(rbenv versions --bare) =~ $1 ]]; then
+if ! rbenv versions --bare | grep -q -x "$1"; then
 	echo "Installing ruby $1"
 	rbenv install $1 || (brew upgrade ruby-build && rbenv install $1)
 	rbenv global $1
