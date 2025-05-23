fa765c7e81ef9882006d83ad53b3cc8c044f51e2
Dan Federman <dfed@me.com>
2025-05-22 21:56:07 -0700

    Source of truth for ruby version

--- ruby/install_ruby.sh
+++ ruby/install_ruby.sh
@@ -8,8 +8,8 @@ echo "Inspecting Ruby…"
 # This version of ruby works well with ARM-based Mac machines.
 # 3.4.4 was the latest stable at the time of writing.
 # We need Ruby before we install our Gems.
-if [[ ! $(rbenv versions --bare) =~ 3.4.4 ]]; then
-	echo "Installing ruby 3.4.4"
-	rbenv install 3.4.4
-	rbenv global 3.4.4
+if [[ ! $(rbenv versions --bare) =~ $1 ]]; then
+	echo "Installing ruby $1"
+	rbenv install $1
+	rbenv global $1
 fi
